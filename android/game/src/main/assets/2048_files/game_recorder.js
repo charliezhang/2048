cef8ee059ec91cbf498373aa06d42bb121a6f4ec
Nicholas <nicholas.zhaoyu@gmail.com>
2014-03-19 23:02:50 -0700

    Update link

--- android/game/src/main/assets/2048_files/game_recorder.js
+++ android/game/src/main/assets/2048_files/game_recorder.js
@@ -1,17 +1,1 @@
-// Record game states history for validation.
-
-function GameRecorder(seed) {
-  this.moves = [];
-  this.seed = seed;
-}
-
-GameRecorder.prototype.recordMove = function(direction) {
-  this.moves.push(direction);
-}
-
-GameRecorder.prototype.serialize = function() {
-  return {
-    'moves': this.moves.join(),
-    'seed': this.seed,
-  };
-}
+../../../../../../libs/game_recorder.js
